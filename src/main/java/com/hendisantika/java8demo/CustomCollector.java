5c397027615f2fe795615fe48018c4c6d6f28e90
hendisantika <hendisantika@yahoo.co.id>
2019-07-30 21:20:46 +0700

    #6 - Add StudentCollector Class

--- src/main/java/com/hendisantika/java8demo/CustomCollector.java
+++ src/main/java/com/hendisantika/java8demo/CustomCollector.java
@@ -1,5 +1,13 @@
 package com.hendisantika.java8demo;
 
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : java8-demo
@@ -24,6 +32,33 @@ class Student {
         return new Student(this.marks = this.marks + s.marks);
     }
 
+}
+
+class StudentCollector implements Collector<Student, Student, Student> {
+    @Override
+    public Supplier<Student> supplier() {
+        return Student::new;
+    }
+
+    @Override
+    public BiConsumer<Student, Student> accumulator() {
+        return Student::addMarks;
+    }
+
+    @Override
+    public BinaryOperator<Student> combiner() {
+        return Student::addMarks;
+    }
+
+    @Override
+    public Function<Student, Student> finisher() {
+        return Function.identity();
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return EnumSet.of(IDENTITY_FINISH);
+    }
 }
 
 public class CustomCollector {
