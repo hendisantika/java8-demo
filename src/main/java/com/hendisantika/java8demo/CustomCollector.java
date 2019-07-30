9ab467f8f17c5d4ea28a131f14f034e92ca3101c
hendisantika <hendisantika@yahoo.co.id>
2019-07-30 21:20:13 +0700

    #5 - Add Student Class

---
+++ src/main/java/com/hendisantika/java8demo/CustomCollector.java
@@ -0,0 +1,32 @@
+package com.hendisantika.java8demo;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : java8-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-30
+ * Time: 21:19
+ */
+
+class Student {
+    int marks;
+
+    Student() {
+    }
+
+    public Student(int i) {
+        this.marks = i;
+    }
+
+    Student addMarks(Student s) {
+        return new Student(this.marks = this.marks + s.marks);
+    }
+
+}
+
+public class CustomCollector {
+}
+
+
