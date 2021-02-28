02221ac0e966388186979fca909ae5e82508b3e5
Hendi Santika <hendisantika@gmail.com>
2021-03-01 05:50:38 +0700

    #23 - Add MapReduceExample

---
+++ src/main/java/com/hendisantika/java8demo/mapreduce/MapReduceExample.java
@@ -0,0 +1,21 @@
+package com.hendisantika.java8demo.mapreduce;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : java8-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/03/21
+ * Time: 05.47
+ */
+public class MapReduceExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(10, 2, 5, 8, 7, 3, 15);
+
+        List<String> words = Arrays.asList("corejava", "spring", "hibernate");
+    }
+}
